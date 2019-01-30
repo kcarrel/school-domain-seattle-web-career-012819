fc69e151afee6b0ce3e93e5f95d6b9794542804e
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-30 17:39:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :roster, :student_name, :school_name
+
+  def initialize(school_name)
+    @school_name = name 
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(student_name, grade)
+    if @roster[grade] == false 
+      @roster[grade] << student_name
+    else
+      @roster[grade] = name 
+    end 
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+  
+  end 
+end 
